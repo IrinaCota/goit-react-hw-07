a36c7160f62e5d95f50685ba68c994ae55282ae7
IrinaCota <150836304+IrinaCota@users.noreply.github.com>
2024-06-28 20:46:27 +0100

    Initial commit

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,59 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useId } from "react";
+import * as Yup from "yup";
+import css from "./ContactForm.module.css";
+import { useDispatch } from "react-redux";
+import { addContact } from "../../redux/contactsSlice";
+import { nanoid } from "nanoid";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required("Required")
+    .min(3, "Too short")
+    .max(50, "Too long"),
+  number: Yup.string()
+    .required("Required")
+    .min(3, "Too short")
+    .max(50, "Too long"),
+});
+
+export default function ContactForm() {
+  const dispatch = useDispatch();
+
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        number: '',
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(values, actions) => {
+        dispatch(
+          addContact({
+            id: nanoid(),
+            name: values.name,
+            number: values.number,
+          })
+        );
+        actions.resetForm();
+      }}
+    >
+      <Form className={css.form}>
+        <label htmlFor={nameFieldId}>Name</label>
+        <Field name="name" type="text" id={nameFieldId} />
+        <ErrorMessage className={css.error} name="name" component="span" />
+
+        <label htmlFor={numberFieldId}>Number</label>
+        <Field name="number" type="text" id={numberFieldId} />
+        <ErrorMessage className={css.error} name="number" component="span" />
+
+        <button className={css.btn} type="submit">
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
+}
